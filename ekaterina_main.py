dc29cfb44f58f892a48c16f08231625cbb33b492
lagutinmichael <mishamedved2000@yandex.com>
2023-02-07 23:46:48 +0500

    add requests_menu

--- ekaterina_main.py
+++ ekaterina_main.py
@@ -29,11 +29,31 @@ def admin_check_command(message):
     command = message.text
     command_list = ['Запросы', 'Вопросы', 'Статистика', 'Рассылка']
     if command in command_list:
-        if command == 'Запросы':
+        if command == command_list[0]:
             bot.send_message(message.from_user.id, 'Выберите действие с Запросами', reply_markup=ekaterina_buttons.admin_get_requests())
             bot.register_next_step_handler(message, admin_requests_menu)
-        elif command == 'Вопросы':
-            pass
+        elif command == command_list[1]:
+            bot.send_message(message.from_user.id, 'Выберите действие с Вопросами', reply_markup=ekaterina_buttons.admin_get_question())
+            bot.register_next_step_handler(message, admin_question_menu)
+        elif command == command_list[2]:
+            bot.send_message(message.from_user.id, 'Какую статистику вы хотите узнать?', reply_markup=ekaterina_buttons.admin_get_statistics())
+            bot.register_next_step_handler(message, admin_statistics_menu)
+        elif command == command_list[3]:
+            bot.send_message(message.from_user.id, 'Выберите, кому хотите отрпавить рассылку', reply_markup=ekaterina_buttons.admin_send_message())
+            bot.register_next_step_handler(message, admin_send_message)
+        else:
+            bot.send_message(message.from_user.id, 'Команда не найдена. \nПопробуйте еще раз или свяжитесь с разработчиком бота')
+            bot.register_next_step_handler(message, admin_check_command)
     else:
         bot.send_message(message.from_user.id, 'Команда не распознана, попробуйте другую', reply_markup=ekaterina_buttons.admin_main())
         bot.register_next_step_handler(message, admin_check_command)
+
+def admin_requests_menu(message):
+    command = message.text
+    command_list = ['Получить вопрос по ID', 'Получить вопрос по имени', 'Получить вопрос по username', 'Назад', 'Отмена']
+
+    if command in command_list:
+        pass
+    else:
+        bot.send_message(message.from_user.id, 'Команда не распознана, попробуйте другую или можете вернуться в главное меню', reply_markup=ekaterina_buttons.cancel())
+        bot.register_next_step_handler(message, admin_requests_menu)
